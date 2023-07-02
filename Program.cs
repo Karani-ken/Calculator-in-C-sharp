8455e7d0d3e3c86d0d9abb2eacfe1cec34e9a0bb
Kenneth Karani <Kenneth Karani@KARANI-KEN>
2023-07-02 17:25:40 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_project.methods;
+namespace Calculator_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculator = new Calculate();
+            int firstNum;
+            int secondNum;
+            string Operator;
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Enter the operator either: +/-/*//");
+            Operator=Console.ReadLine();
+            if(Operator == "+")
+            {
+                Console.WriteLine("Enter first Number");
+                firstNum = Int32.Parse( Console.ReadLine());
+                Console.WriteLine("Enter second Number");
+                secondNum= Int32.Parse( Console.ReadLine());
+                int result = calculator.Add(firstNum, secondNum);
+                Console.WriteLine($"Your answer is: {result}");
+               
+
+            }else if(Operator == "-")
+            {
+                Console.WriteLine("Enter first Number");
+                firstNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second Number");
+                secondNum = Int32.Parse(Console.ReadLine());
+               int result = calculator.Subtract(firstNum, secondNum);
+                Console.WriteLine($"Your answer is: {result}");
+            }
+            else if (Operator == "*")
+            {
+                Console.WriteLine("Enter first Number");
+                firstNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second Number");
+                secondNum = Int32.Parse(Console.ReadLine());
+                int result = calculator.Multiply(firstNum, secondNum);
+                Console.WriteLine($"Your answer is: {result}");
+            }
+            else if (Operator == "/")
+            {
+                Console.WriteLine("Enter first Number");
+                firstNum = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second Number");
+                secondNum = Int32.Parse(Console.ReadLine());
+                double result = calculator.Divide(firstNum, secondNum);
+                Console.WriteLine($"Your answer is: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator!!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ methods/Calculate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_project.methods
+{
+    public class Calculate
+    {
+        public int Num1;
+        public int Num2;
+        public int Add(int x, int y)
+        {
+            int result;
+            Num1 = x;
+            Num2 = y;
+            result = Num1 + Num2;
+            return result;
+        }
+        public int Subtract(int x, int y)
+        {
+            int result;
+            Num1=x; Num2=y;
+            result = Num1 - Num2;
+            return result;
+        }
+        public int Multiply(int x, int y)
+        {
+            int result;
+            Num1=x; Num2=y;
+            result = Num1 * Num2;
+            return result;
+        }
+        public double Divide(int x, int y)
+        {
+            double result;
+            Num1=x; Num2=y;
+            result = Num1 / Num2;
+            return result;
+        }
+    }
+}
